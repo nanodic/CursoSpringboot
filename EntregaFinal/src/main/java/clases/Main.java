3fb372b9e20c413edb1d1fef0b6ae39e524f2f0a
nanodic <nahuel.diciocco@gmail.com>
2024-06-18 00:13:34 -0300

    Cambio en main - no anda

--- EntregaFinal/src/main/java/clases/Main.java
+++ EntregaFinal/src/main/java/clases/Main.java
@@ -5,6 +5,9 @@ import gestores.GestorInvoice;
 import gestores.GestorInvoiceDetails;
 import gestores.GestorProducts;
 
+import java.util.Date;
+
+
 public class Main {
 
     public static void main(String[] args) {
@@ -19,8 +22,19 @@ public class Main {
             products.create("Yerba 1 kilo", "112", 55, 2300.00);
             products.create("Azucar 1 Kilo", "113", 25, 1100.00);
             products.create("Cafe 800gr", "114", 10, 2800.00);
+
+            Date fecha_creacion = new Date();
+
             GestorInvoice invoice = new GestorInvoice();
             Clients clients1 =  clients.readById(1);
+            invoice.create(clients1.getId(),fecha_creacion,0.00);
+
+            Invoice invoice1 = invoice.readById(1);
+            Products products1 = products.readById(1);
+
+            GestorInvoiceDetails invoiceDetails = new GestorInvoiceDetails();
+            invoiceDetails.create(invoice1.getId(),0,products1.getId(),products1.getPrice());
+
         }
         catch (Exception exception)
         {
